70d6f984bfc6911ccc711cb3ff7efcd1af163737
michalgabor <34399340+michalgabor@users.noreply.github.com>
2019-09-10 14:16:57 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@ echo "Adding upstream host $PROXY_UPSTREAM_HOST to the NGINX config"
 sed -i -e "s,___PROXY_UPSTREAM_HOST___,$PROXY_UPSTREAM_HOST,g" /etc/nginx/conf.d/default.conf
 echo "Adding origin $ORIGIN to the NGINX config"
 sed -i -e "s,___ORIGIN___,$ORIGIN,g" /etc/nginx/conf.d/default.conf
+echo "Adding origin $ORIGIN2 to the NGINX config"
+sed -i -e "s,___ORIGIN2___,$ORIGIN2,g" /etc/nginx/conf.d/default.conf
 echo "Adding custom config $CONFIG1 to the NGINX config"
 sed -i -e "s,#___CONFIG1___,$CONFIG1,g" /etc/nginx/conf.d/default.conf
 echo "Adding manual cors path $MANUAL_CORS_PATH to the NGINX config"
